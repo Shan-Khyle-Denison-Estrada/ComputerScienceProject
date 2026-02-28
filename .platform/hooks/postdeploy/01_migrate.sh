9f1687584c9293f79cb2534758116ac070ceb885
Shan-Khyle-Denison-Estrada <145841475+Shan-Khyle-Denison-Estrada@users.noreply.github.com>
2026-02-28 08:37:53 +0800

    Update 01_migrate.sh

--- .platform/hooks/postdeploy/01_migrate.sh
+++ .platform/hooks/postdeploy/01_migrate.sh
@@ -5,3 +5,6 @@ source /opt/elasticbeanstalk/deployment/env
 
 # Run migrations and safely seed the database
 php artisan migrate --seed --force
+
+# Create the public storage shortcut for images
+php artisan storage:link --force
