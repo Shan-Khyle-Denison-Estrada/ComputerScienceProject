271a8ef84a4c1f628d023ef9a7abf470e088c7b9
Shan-Khyle-Denison-Estrada <145841475+Shan-Khyle-Denison-Estrada@users.noreply.github.com>
2026-03-05 08:23:27 +0800

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -11,7 +11,7 @@ Artisan::command('inspire', function () {
 // Run the auto-renewal check every day at midnight
 // Schedule::command('franchise:auto-renew')->daily();
 Schedule::command('franchise:auto-renew')
-    ->dailyAt('8:20');
+    ->dailyAt('8:35');
 
 Schedule::command('assessments:update-penalties')
     ->daily()
